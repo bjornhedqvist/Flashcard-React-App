b07c861182d580dddbeebd82ebba77d8947c790b
Björn Hedqvist <bjornhedqvist22@gmail.com>
2022-11-29 14:31:35 -0800

    too few cards fix
    added alternative screen for when deck length is below 3, with a redirect button to adding a new card

--- src/Layout/Study.js
+++ src/Layout/Study.js
@@ -3,9 +3,9 @@ import { useState, useEffect } from "react";
 import { useParams, NavLink, useHistory } from "react-router-dom";
 import { readDeck } from "../utils/api";
 
-export default function Study({ decks }) {    
+export default function Study({ decks }) {
   const { deckId } = useParams();
-  const history = useHistory()
+  const history = useHistory();
 
   const [deck, setDeck] = useState([]);
   const [cardSet, setCardSet] = useState([]);
@@ -25,18 +25,22 @@ export default function Study({ decks }) {
     loadThisDeck();
   }, [deckId]);
 
-  function nextButtonClickHandler (){
-      if ((cardNumber+1) === cardSet.length){
-        if(window.confirm('Restart cards? \n \n Click "Cancel" to return to the home page')){
-            setCardNumber(0)
-        }else{
-            history.push("/")
-        }
-      }else{
-        setCardNumber(cardNumber+1);
-        setFlippedToFront(!flippedToFront);
+  function nextButtonClickHandler() {
+    if (cardNumber + 1 === cardSet.length) {
+      if (
+        window.confirm(
+          'Restart cards? \n \n Click "Cancel" to return to the home page'
+        )
+      ) {
+        setCardNumber(0);
+      } else {
+        history.push("/");
       }
-  };
+    } else {
+      setCardNumber(cardNumber + 1);
+      setFlippedToFront(!flippedToFront);
+    }
+  }
 
   return cardSet.length >= 3 ? (
     <div>
@@ -56,7 +60,7 @@ export default function Study({ decks }) {
             </li>
           </ol>
         </nav>
-        <h2 className="text-center">Study: {deck.name}</h2>
+        <h2 className="text-left ml-5">Study: {deck.name}</h2>
       </div>
 
       <div>
@@ -100,6 +104,32 @@ export default function Study({ decks }) {
       </div>
     </div>
   ) : (
-    <p>FIXME what displays when cards are less than 3?</p>
+    <div>
+      <nav aria-label="breadcrumb" className="ml-5 mr-5">
+        <ol className="breadcrumb">
+          <li className="breadcrumb-item">
+            <NavLink to={"/"}>
+              <span className="oi oi-home mr-1"></span>Home
+            </NavLink>
+          </li>
+          <li className="breadcrumb-item">
+            <NavLink to={"/decks/deckId"}>{deck.name}</NavLink>
+          </li>
+          <li className="breadcrumb-item active" aria-current="page">
+            Study
+          </li>
+        </ol>
+      </nav>
+      <h2 className="text-left ml-5">Study: {deck.name}</h2>
+      <h4 className="text-left ml-5">Not enough cards.</h4>
+      <p className="text-left ml-5">
+        You need at least 3 cartds to study. There are {cardSet.length} cards in
+        this deck.
+      </p>
+      <NavLink to={"/cards/new"} role="button" className="btn btn-primary ml-5">
+        <span className="oi oi-plus mr-1"></span>
+        Create Deck
+      </NavLink>
+    </div>
   );
 }
